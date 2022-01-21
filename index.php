606e0f323e9643868a601ab1a62537a838a19bf2
ME9799 <93213577+ME9799@users.noreply.github.com>
2022-01-21 12:57:21 +0300

    Add files via upload
    http://localhost/Land_Easy/index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Land Easy</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+	<section class="">
+		<?php
+			include 'header.php';
+		?>
+
+			<section class="caption">
+				<h2 class="caption" style="text-align: center">Don't wait to buy land</h2>
+				<h3 class="properties" style="text-align: center">Buy land and wait!</h3>
+			</section>
+	</section><!--  end hero section  -->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <!-- <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/0.9.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/00.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/6.7.jpg" class="d-block w-100" alt="...">
+    </div> -->
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+	<section class="listings">
+		<div class="wrapper">
+			<ul class="properties_list">
+			<?php
+						include 'includes/config.php';
+						$sel = "SELECT * FROM cars WHERE status = 'Available'";
+						$rs = $conn->query($sel);
+						while($rws = $rs->fetch_assoc()){
+			?>
+				<li>
+					<a href="book land.php?id=<?php echo $rws['car_id'] ?>">
+						<img class="thumb" src="landeasy/<?php echo $rws['image'];?>" width="300" height="200">
+					</a>
+					<span class="price"><?php echo 'Kshs.'.$rws['hire_cost'];?></span>
+					<div class="property_details">
+						<h1>
+							<a href="book land.php?id=<?php echo $rws['car_id'] ?>"><?php echo 'Land location>'.$rws['car_type'];?></a>
+						</h1>
+						<h2>Land Type: <span class="property_size"><?php echo $rws['car_name'];?></span></h2>
+					</div>
+				</li>
+			<?php
+				}
+			?>
+			</ul>
+		</div>
+	</section>	<!--  end listing section  -->
+
+	<footer>
+		<div class="wrapper footer">
+			<ul>
+				<li class="links">
+					<ul>
+						<li>OUR COMPANY</li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Terms</a></li>
+						<li><a href="#">Policy</a></li>
+						
+					</ul>
+				</li>
+
+				<li class="links">
+					<ul>
+						<li>CONTACTS</li>
+						<li><a href="#">+254796320640</a></li>
+						<li><a href="#">...</a></li>
+						<li><a href="#">...</a></li>
+						
+					</ul>
+				</li>
+
+				<li class="links">
+					<ul>
+						<li>OUR LAND TYPES</li>
+						<li><a href="#">Agricultural Land.</a></li>
+						<li><a href="#">Recreational land</a></li>
+						<li><a href="#">Residential land</a></li>
+						<li><a href="#">Commercial land</a></li>
+					</ul>
+				</li>
+
+				<li class="about">
+					<p>Land Easy is where affordability meets value for money and the dream of owning land comes true. "Buy land and wait, don't wait and buy"</p>
+					<ul>
+						<li><a href="//www.facebook.com/" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/sonko" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/+sonko" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Land Easy</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<style type="text/css">
+		.status{
+			font-size: 20px;
+		}
+		.txt{
+			width: 600px;
+			height: 200px;
+		}
+	</style>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+	<section class="">
+		<?php
+			include 'header.php';
+		?>
+
+			<section class="caption">
+				<h2 class="caption" style="text-align: center">Don't wait to buy land</h2>
+				<h3 class="properties" style="text-align: center">Buy land and wait!</h3>
+			</section>
+	</section><!--  end hero section  -->
+
+
+	<section class="listings">
+		<div class="wrapper">
+		<h2 style="text-align: center; color:#CC0000">Thank you for contacting us. We will give you a call shortly.</h2>
+			<ul class="properties_list">
+			
+			</ul>
+		</div>
+	</section>	<!--  end listing section  -->
+
+	<footer>
+		<div class="wrapper footer">
+			<ul>
+				<li class="links">
+					<ul>
+						<li>OUR COMPANY</li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Terms</a></li>
+						<li><a href="#">Policy</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</li>
+
+				<li class="links">
+					<ul>
+						<li>OTHERS</li>
+						<li><a href="#">...</a></li>
+						<li><a href="#">...</a></li>
+						<li><a href="#">...</a></li>
+						<li><a href="#">...</a></li>
+					</ul>
+				</li>
+
+				<li class="links">
+					<ul>
+						<li>OUR LAND TYPES</li>
+						<li><a href="#">Agricultural Land</a></li>
+						<li><a href="#">Recreational Land</a></li>
+						<li><a href="#">Residential Land</a></li>
+						<li><a href="#">Commercial Land</a></li>
+					</ul>
+				</li>
+
+				<li class="about">
+					<p>Land Easy is where affordability meets value for money and the dream of owning land comes true. "Buy land and wait, don't wait and buy"</p>
+					<ul>
+						<li><a href="http://facebook.com/sonko" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/sonko" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/+sonko" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
